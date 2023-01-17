47e517ba282eb8ed68816a5d9bcbfb697475cab8
NewOS <13276545119@phone.com>
2023-01-17 14:07:47 +0800

    .

--- pyscript/core/GUI.py
+++ pyscript/core/GUI.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.tix as tix
+import tkinter.scrolledtext as stext
 
 import os.path as path
 from core.settings import Settings
@@ -11,11 +12,13 @@ class App():
   def __init__(self,config,root=tix.Tk()):
     self.root=root
     self.config=config
-    self.lang = Settings(path.join(
+    self.langFull = Settings(path.join(
       self.config.getConfig("lang.home"),self.config.getConfig("lang.default")
-      )).getSubSettings(["GUI",])
+      ))
+    self.lang = self.langFull.getSubSettings(["GUI",])
     
     self.root.mconf = self.config.getSubSettings(["GUI","main"])
+    self.winconf = self.config.getSubSettings(["GUI","window"])
     self.root.title(self.lang.gc("main.title"))
     self.root.geometry("{0}x{1}+{2}+{3}".format(
       self.root.mconf.gc("width"),
@@ -34,6 +37,22 @@ class App():
     #主页面
     self.phome = ttk.Frame(self.nb)
     self.nb.add(self.phome,text=self.lang.gc("home.title"))
+    
+    # ------------ 软件介绍区 ----------------------------
+    self.phome.descf = tk.LabelFrame(self.phome,text=self.lang.gc("home.descTitle"))
+    self.phome.descf.grid(row=1,column=1)
+    self.phome.descf.st = stext.ScrolledText(self.phome.descf,width=14,height=10,wrap=tk.WORD)
+    self.phome.descf.st.insert(tk.END,self.langFull.gc("description"))
+    self.phome.descf.st.pack(fill=tk.BOTH)
+    
+    # ------------ 按钮控制区 ----------------------------
+    self.phome.btnf = ttk.LabelFrame(self.phome,text=self.lang.gc("home.btnFrame"))
+    self.phome.btnf.grid(row=2,column=1,sticky="nsew")
+    tk.Button(self.phome.btnf,text=self.lang.gc("home.btns.run")).pack(fill=tk.X)
+    tk.Button(self.phome.btnf,text=self.lang.gc("home.btns.exit"),command=lambda:self.root.destroy()).pack(fill=tk.X)
+    
+    # ------------ 文件输入区 ----------------------------
+    
   def info(self):
     #信息页面
     self.pinfo = ttk.Frame(self.nb)
@@ -41,9 +60,39 @@ class App():
     
     self.infoshower = InfoShower(self.pinfo)
     self.infoshower.getContainer().pack(fill=tk.BOTH)
+    
+    infoma = {
+      self.lang.gc("info.shower.author"):
+        self.readonlyMultiText(self.infoshower,"DouNsM"),
+      self.lang.gc("info.shower.version"):
+        self.readonlyMultiText(self.infoshower,"{0}\n{1}".format(
+          self.lang.gc("info.shower.vver")+VERSION["version"],
+          self.lang.gc("info.shower.vdat")+VERSION["date"]
+        ))
+    }
+    self.infoshower.refresh(infoma)
+  
+  def readonlyMultiText(self,infos,text):
+    s = stext.ScrolledText(infos.getRoot(),
+      width=self.winconf.gc("scrolledText.width"),
+      height=self.winconf.gc("scrolledText.height"),
+      
+      )
+    s.insert(tk.END,text)
+    return s
+  
   def uplog(self):
     #更新日志界面
     self.puplog = ttk.Frame(self.nb)
     self.nb.add(self.puplog,text=self.lang.gc("uplog.title"))
+    self.uplogshower = InfoShower(self.puplog)
+    self.uplogshower.getContainer().pack(fill=tk.BOTH)
+    logs = {}
+    rev_logs = list(UPDATE_LOG.items());
+    rev_logs.reverse()
+    for (k,v) in rev_logs:
+      logs[k]=self.readonlyMultiText(self.uplogshower,v)
+    self.uplogshower.refresh(logs)
+    
   def mainloop(self):
     self.root.mainloop()
--- pyscript/core/win/InfoShower.py
+++ pyscript/core/win/InfoShower.py
@@ -14,6 +14,16 @@ class InfoShower():
     self.klist.grid(row=1,column=1)
     self.vfram = tk.Frame(self.frame)
     self.vfram.grid(row=1,column=2)
+    tk.Label(self.vfram).pack()
+    
+    self.klist.bind("<ButtonRelease-1>",lambda e:self.showItem(e))
+  def showItem(self,event,speckey=False):
+    key = speckey if speckey else self.klist.get(self.klist.curselection())
+    for ele in self.cont.values():
+      ele.pack_forget()
+    self.cont[key].pack(fill=tk.BOTH)
+  def getRoot(self):
+    return self.vfram
   def getContainer(self):
     return self.frame
   def refresh(self,cont):
@@ -21,9 +31,11 @@ class InfoShower():
     @params cont => {str <show_name>:tk.Frame <content>}
     '''
     self.cont = cont
-    self.keys_arr = self.cont.keys()
+    # print(self.cont)
+    self.keys_arr = list(self.cont.keys())
     self.keys.set(self.keys_arr)
-  def 
+    self.showItem(None,speckey=list(self.cont.keys())[0])
+    
     
     
     
